78a6b7046ca4f05522731da0d872b8ed09438733
pratham meena <yashmeena274@gmail.com>
2024-05-12 12:24:25 +0530

    adding framer motion

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './index.css'
 import { nanoid } from 'nanoid';
 import gsap from 'gsap';
 import { useGSAP } from '@gsap/react';
+import { motion } from "framer-motion"
 
 
 const App = () => {
@@ -59,12 +60,19 @@ const App = () => {
     })
   },[strokeDashoffset])
 
+  const parent = useRef(null)
 
   return (
     <>
-      <div className="main flex items-center bg-black justify-center h-screen w-full p-10">
+      <div className="main items-center bg-zinc-800 justify-center h-screen w-[100vw] p-2 overflow-hidden">
+          <nav className='fixed z-[99]'>
+          <form onSubmit={submitHandeler} className='w-[50vw] flex items-center gap-2 rounded-full border p-2 overflow-hidden'>
+            <input onChange={(e) => setinput(e.target.value)} value={input} type="text" placeholder='Task' className=' px-2 text-xl outline-none text-white py-1 bg-transparent  w-full ' />
+            <button type='submit' className=' bg-zinc-900 rounded-full px-2 aspect-square text-2xl text-white font-black flex items-center justify-center'><i className="ri-add-line"></i></button>
+          </form>
+          </nav>
 
-        <div className="left w-1/2 h-full flex flex-col gap-10 items-center justify-center">
+        <div className="left-0 relative w-1/2 h-full flex flex-col gap-10 items-center justify-center overflow-hidden">
           <div className="head w-2/3 h-[35vh] bg-zinc-900 rounded-xl py-6 flex gap-4 items-center justify-center">
             <h1 className='text-3xl text-white font-medium'>Progress</h1>
             <div className="progress w-1/2 h-full flex items-center justify-center relative">
@@ -75,21 +83,23 @@ const App = () => {
               <h1 className='text-2xl font-medium text-white'>{tasks.filter(t=> t.complete).length}/{tasks.length}</h1>
             </div>
           </div>
-          <form onSubmit={submitHandeler} className='w-2/3 flex items-center gap-2 rounded-full border p-2'>
-            <input onChange={(e) => setinput(e.target.value)} value={input} type="text" placeholder='Task' className='px-2 text-xl outline-none text-white py-1 bg-transparent  w-full ' />
-            <button type='submit' className='bg-zinc-900 rounded-full px-2 aspect-square text-2xl text-white font-black flex items-center justify-center'><i className="ri-add-line"></i></button>
-          </form>
         </div>
 
 
-        <div className="right w-[25vw] h-full flex flex-col items-center justify-center gap-4">
-          <h1 className='text-white w-full text-3xl font-medium mb-4 text-center'>Tasks</h1>
-          <div className="tasks w-full flex flex-col gap-2 overflow-y-auto h-[60vh]">
+        <div ref={parent} className="right absolute top-0 pt-20 w-full h-screen flex flex-col items-center justify-center gap-4 overflow-hidden">
+          <div  className="tasks w-full flex gap-2 overflow-hidden h-screen">
             {tasks.length == 0 ? [<h1 key={nanoid()} className='text-white font-medium text-3xl capitalize text-center'>no task yet</h1>] : tasks.map((task,index) => {
-              return (<div key={task.id}  className="task flex items-center bg-zinc-900  rounded-full px-4 py-2 gap-2">
-                <i onClick={() => taskCompleted(index)} className={task.complete ? completedClass : incompletedClass}></i><h1 className={task.complete ? completeName : incompleteName}>{task.name}</h1>
-                <i onClick={()=> deleteHandler(index)} className='ri-close-line text-white'></i>
-              </div>)
+              return (<motion.div
+                key={task.id}
+                drag // Enable drag functionality
+                dragConstraints={parent}
+                className="task flex items-center bg-zinc-900 rounded-xl w-[10vw] h-[8vw] px-4 py-2 gap-2"
+                style={{ cursor: 'grab' }} // Set cursor to indicate draggable element
+              >
+                <i onClick={() => taskCompleted(index)} className={task.complete ? completedClass : incompletedClass}></i>
+                <h1 className={task.complete ? completeName : incompleteName}>{task.name}</h1>
+                <i onClick={() => deleteHandler(index)} className='ri-close-line text-white'></i>
+              </motion.div>)
             })}
           </div>
 
